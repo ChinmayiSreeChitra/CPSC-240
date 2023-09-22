73e6c39145a2572e96411afbf318a8bbc1779bd5
ChinmayiSreeChitra <109444486+ChinmayiSreeChitra@users.noreply.github.com>
2023-09-21 23:52:21 -0700

    Update ex3_multiplication1.asm

--- ex3_multiplication1.asm
+++ ex3_multiplication1.asm
@@ -1,19 +1,17 @@
 section .data
-    num1 dd 300000      ; num1 = 300,000
-    num2 dd 400000      ; num2 = 400,000
-    product dq 0        ; product = 0 (64-bit)
+    num1 dd 300000        ; num1 = 300,000
+    num2 dd 400000        ; num2 = 400,000
+    product dq 0          ; product = 0 (64-bit)
 
 section .text
 global _start
 
 _start:
-    mov rax, [num1]     ; Load num1 into rax
-    mov rbx, [num2]
-    mov rcx, [num2]     ; Load num2 into rcx
-    imul rax, rcx       ; Multiply rax by rcx
-    mov [product], rax  ; Store the result in product
+    mov eax, [num1]      ; Load num1 into eax
+    mul dword [num2]     ; Multiply eax by num2 (32-bit multiplication)
+    mov [product], rax   ; Store the 64-bit result in product
 
     ; Exit the program
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; exit status 0
+    mov eax, 60          ; syscall number for exit
+    xor edi, edi         ; exit status 0
     syscall
